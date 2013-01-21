3bb594e643821b873b850e956157acf5814e653a
Jorge Pablo Tordoya Rojas <keizerberos@hotmail.com>
2013-01-21 17:28:19 -0400

    errores

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ colors.sort(function(a,b) { return Math.random() > 0.5; } );
 var server = http.createServer(function(request, response) {
     // Not important for us. We're writing WebSocket server, not HTTP server
 });
-console.log(" Server is listening on port ..... grrr" );
+//console.log(" Server is listening on port ..... grrr" );
 server.listen(webSocketsServerPort, function() {
     console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
 });
